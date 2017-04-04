c060377a768826787193bf1a6a2cddbf49df2f54
bpatkins <bpatkins@users.noreply.github.com>
2017-04-03 19:37:17 -0500

    Clearing source code out of this repo and moving it to bpatkins.src and just commit artifacts to this repo for bpatkins.github.io

--- src/app/content-view/content-view.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-@Component({
-    selector: 'app-content-view',
-    templateUrl: './content-view.component.html',
-    styleUrls: ['./content-view.component.css']
-})
-export class ContentViewComponent { }
